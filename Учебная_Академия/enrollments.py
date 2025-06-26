84bddfb7df4137a9d4c0731b8df88c4a638c2119
facefd <face_shot@mail.ru>
2025-06-26 17:45:03 +0300

    Update enrollments.py

--- Учебная_Академия/enrollments.py
+++ Учебная_Академия/enrollments.py
@@ -1,5 +1,7 @@
+
 # Новиков никита
 from tkinter import ttk, messagebox, END
+from database import add_enrollment
 
 
 class EnrollmentManager:
@@ -9,7 +11,7 @@ class EnrollmentManager:
 
         self.student_id_label = ttk.Label(master, text="ID студента:")
         self.student_id_label.grid(row=0, column=0, padx=5, pady=5, sticky='w')
-        self.student_id_entry = ttk.Entry(master, width=30)
+        self.student_id_entrya= ttk.Entry(master, width=30)
         self.student_id_entry.grid(row=0, column=1, padx=5, pady=5)
 
         self.course_id_label = ttk.Label(master, text="ID курса:")
@@ -17,15 +19,14 @@ class EnrollmentManager:
         self.course_id_entry = ttk.Entry(master, width=30)
         self.course_id_entry.grid(row=1, column=1, padx=5, pady=5)
 
-        self.register_button = ttk.Button(master, text="Зарегистрировать ➕", command=self.зарегистрировать)
+        self.register_button = ttk.Button(master, text="Зарегистрировать ➕", command=self.register_student)
         self.register_button.grid(row=2, column=0, columnspan=2, padx=5, pady=5, sticky='ew')
 
-    def зарегистрировать(self):
+    def register_student(self):
         try:
             student_id = int(self.student_id_entry.get())
             course_id = int(self.course_id_entry.get())
-            from database import добавить_запись
-            добавить_запись(student_id, course_id)
+            add_enrollment(student_id, course_id)
             self.student_id_entry.delete(0, END)
             self.course_id_entry.delete(0, END)
             messagebox.showinfo("✅ Успех", "Студент зарегистрирован на курс!")
